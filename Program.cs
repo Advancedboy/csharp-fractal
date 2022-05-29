921f3c00a0c29a0bde1b6bd412007dcd410c5764
Pashka <51019643+Advancedboy@users.noreply.github.com>
2022-05-29 19:36:26 +0300

    Recursion in C#

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        public static int PrintHello(string name, string surname)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return 0;
+            }
+            if (string.IsNullOrEmpty(surname))
+            {
+                return 0;
+            }
+            Console.WriteLine($"Hello {name} {surname}.");
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(10));
+            Console.WriteLine(FactorialCycle(5));
+            Console.ReadLine();
+
+        }
+
+        public static int Factorial(int value)
+        {
+            if (value <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return value * Factorial(value - 1);
+            }
+
+        }
+
+        public static int FactorialCycle(int value)
+        {
+            int i;
+            int res;
+            res = 1;
+            for (i = 1; i <= value; i++)
+            {
+                res = res * i;
+            }
+            return res;
+        }
+    }
+}
